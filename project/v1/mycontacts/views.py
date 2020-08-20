c26a41c3363b941494ee71a38f1b78aff57b3de0
tasneembegum34 <tasneembegum34@gmail.com>
2020-08-20 20:24:54 +0530

    second version

--- project/v1/mycontacts/views.py
+++ project/v1/mycontacts/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 import operator
 
 def family(requests):
-    return HttpResponse('<h1><ul><li>Mother:9125673990</li><li>Father:7348040387</li><li>Brother:8576967292</li></ul></h1>')
+    return HttpResponse('<h1><ul><li>Mom:9125673990</li><li>Father:7348040387</li><li>Brother:8576967292</li></ul></h1>')
 def friends(requests):
     return HttpResponse('<h1><ul><li>Friend-1:6074245670</li><li>Friend-2:9605745490</li></ul></h1>')
